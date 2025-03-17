30c04cb9eeff47573592f4947a86740d17ba8f36
keshvendra63 <118011825+keshvendra63@users.noreply.github.com>
2025-03-17 11:29:13 +0000

    myweb

--- components/ContactSection.jsx
+++ components/ContactSection.jsx
@@ -74,7 +74,7 @@ const ContactSection = () => {
     setIsSubmitting(true);
     
     try {
-      const res = await fetch(scriptUrl, {
+      const res = await fetch("https://script.google.com/macros/s/AKfycbwkFGspupUlZWh3qhwNqQzB61xbZHBhsScbbgyop-hBlzqZDaWa9odYxX6hS4LpEidbxw/exec", {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify(formData),
